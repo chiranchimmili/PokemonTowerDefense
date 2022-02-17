dff2f936041d780255f7508d99518a3f79f62a40
Jerry <jwu682@gatech.edu>
2022-02-16 23:19:21 -0500

    locked potrait mode and enabled immersive view for start screen

--- app/src/main/java/com/my/pokemontowerdefense/WelcomeScreen.kt
+++ app/src/main/java/com/my/pokemontowerdefense/WelcomeScreen.kt
@@ -4,13 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.content.Intent
-import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class WelcomeScreen : AppCompatActivity() {
+    private fun hideSystemBars() {
+        val windowInsetsController =
+            ViewCompat.getWindowInsetsController(window.decorView) ?: return
+        // Configure the behavior of the hidden system bars
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        // Hide both the status bar and the navigation bar
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        hideSystemBars()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome_screen)
-
 
         val startButton = findViewById<Button>(R.id.start_button)
 
