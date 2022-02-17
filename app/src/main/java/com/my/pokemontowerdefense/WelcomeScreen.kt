9cb727aed3fbc4725f7dcd9b0eccec1a3cdf6c08
Jerry <jwu682@gatech.edu>
2022-02-16 22:33:22 -0500

    UI updates to starting screen

--- app/src/main/java/com/my/pokemontowerdefense/WelcomeScreen.kt
+++ app/src/main/java/com/my/pokemontowerdefense/WelcomeScreen.kt
@@ -3,18 +3,21 @@ package com.my.pokemontowerdefense
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.content.Intent
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class WelcomeScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome_screen)
 
+
         val startButton = findViewById<Button>(R.id.start_button)
 
         val quitButton = findViewById<Button>(R.id.quit_button)
 
         startButton.setOnClickListener{
-            val intent = Intent(this, ConfigScreen::class.java)
+            val intent = Intent(this, testSecondScreen::class.java)
             startActivity(intent)
         }
 
@@ -23,4 +26,5 @@ class WelcomeScreen : AppCompatActivity() {
             System.exit(0)
         }
     }
+    
 }
---
+++ app/src/main/java/com/my/pokemontowerdefense/testSecondScreen.kt
@@ -0,0 +1,11 @@
+package com.my.pokemontowerdefense
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class testSecondScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_second_screen)
+    }
+}
