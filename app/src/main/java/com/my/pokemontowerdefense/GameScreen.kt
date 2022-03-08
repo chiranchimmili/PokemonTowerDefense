4180456363a4b6b0f5609186ab176d47583f5d92
Chiranjeevi <cchimmili3@gatech.edu>
2022-03-07 21:00:22 -0500

    tower placement functionality enhanced

--- app/src/main/java/com/my/pokemontowerdefense/GameScreen.kt
+++ app/src/main/java/com/my/pokemontowerdefense/GameScreen.kt
@@ -1,24 +1,15 @@
 package com.my.pokemontowerdefense
 
-import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.my.pokemontowerdefense.ui.login.ConfigScreen
-import kotlinx.android.synthetic.main.activity_config_screen.*
 import android.annotation.SuppressLint
-import android.graphics.Rect
-import android.view.MotionEvent
 import android.view.View
-import android.view.View.OnTouchListener
 import android.view.ViewGroup
 import android.widget.*
-import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_game_screen.*
 
 
 open class GameScreen() : AppCompatActivity() {
-
-    var state = 0
 
     var player1 = Player()
     var difficulty: String = ""
@@ -28,6 +19,16 @@ open class GameScreen() : AppCompatActivity() {
         set(value) {
             field = value
         }
+
+    var location1_on = false
+    var location2_on = false
+    var location3_on = false
+    var location4_on = false
+    var location5_on = false
+    var location6_on = false
+    var location7_on = false
+    var location8_on = false
+    var location9_on = false
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,10 +58,11 @@ open class GameScreen() : AppCompatActivity() {
         val buyTower2 = findViewById<Button>(R.id.BuyTower2)
         val buyTower3 = findViewById<Button>(R.id.BuyTower3)
 
-
+        // Displays starting money, depending on difficulty
         var moneyView: TextView = findViewById<TextView>(R.id.startingMoney)
         moneyView.text = player1.money.toString()
 
+        // Displays monument health, depending on difficulty
         var healthView: TextView = findViewById<TextView>(R.id.monumentHealth)
         healthView.text = monumentHealth.toString()
 
@@ -81,32 +83,99 @@ open class GameScreen() : AppCompatActivity() {
 
     }
 
+    // placement of towers functionality, can currently place in one of nine spots on screen
     fun placement() {
-
         val location1 = findViewById<Button>(R.id.location1button)
         val location2 = findViewById<Button>(R.id.location2button)
-        location1.visibility = View.VISIBLE
-        location2.visibility = View.VISIBLE
+        val location3 = findViewById<Button>(R.id.location3button)
+        val location4 = findViewById<Button>(R.id.location4button)
+        val location5 = findViewById<Button>(R.id.location5button)
+        val location6 = findViewById<Button>(R.id.location6button)
+        val location7 = findViewById<Button>(R.id.location7button)
+        val location8 = findViewById<Button>(R.id.location8button)
+        val location9 = findViewById<Button>(R.id.location9button)
 
+        val list = arrayListOf(location1, location2, location3, location4, location5, location6,
+            location7, location8, location9)
 
-        location1.setOnClickListener{
-
-            location1.visibility = View.INVISIBLE
-            location2.visibility = View.INVISIBLE
-            placeTower(location2relative)
+        if (!(location1_on)) {
+            location1.visibility = View.VISIBLE
+        }
+        if (!(location2_on)) {
+            location2.visibility = View.VISIBLE
+        }
+        if (!(location3_on)) {
+            location3.visibility = View.VISIBLE
+        }
+        if (!(location4_on)) {
+            location4.visibility = View.VISIBLE
+        }
+        if (!(location5_on)) {
+            location5.visibility = View.VISIBLE
+        }
+        if (!(location6_on)) {
+            location6.visibility = View.VISIBLE
+        }
+        if (!(location7_on)) {
+            location7.visibility = View.VISIBLE
+        }
+        if (!(location8_on)) {
+            location8.visibility = View.VISIBLE
+        }
+        if (!(location9_on)) {
+            location9.visibility = View.VISIBLE
         }
 
-        location2.setOnClickListener{
-
-            location1.visibility = View.INVISIBLE
-            location2.visibility = View.INVISIBLE
+        location1.setOnClickListener {
+            visibilityOff(list)
+            location1_on = true
             placeTower(location1relative)
-
+        }
+        location2.setOnClickListener {
+            visibilityOff(list)
+            location2_on = true
+            placeTower(location2relative)
+        }
+        location3.setOnClickListener {
+            visibilityOff(list)
+            location3_on = true
+            placeTower(location3relative)
+        }
+        location4.setOnClickListener {
+            visibilityOff(list)
+            location4_on = true
+            placeTower(location4relative)
+        }
+        location5.setOnClickListener {
+            visibilityOff(list)
+            location5_on = true
+            placeTower(location5relative)
+        }
+        location6.setOnClickListener {
+            visibilityOff(list)
+            location6_on = true
+            placeTower(location6relative)
+        }
+        location7.setOnClickListener {
+            visibilityOff(list)
+            location7_on = true
+            placeTower(location7relative)
+        }
+        location8.setOnClickListener {
+            visibilityOff(list)
+            location8_on = true
+            placeTower(location8relative)
+        }
+        location9.setOnClickListener {
+            visibilityOff(list)
+            location9_on = true
+            placeTower(location9relative)
         }
     }
 
+    // Currently only places an imageview (charmander), need to make it so it places
+    // a tower class or subclass object, which should also be an imageview
     fun placeTower(view: ViewGroup) {
-
         val imageView = ImageView(this)
         imageView.layoutParams= LinearLayout.LayoutParams(400, 400)
         imageView.x= 20F
@@ -114,8 +183,13 @@ open class GameScreen() : AppCompatActivity() {
         val imgResId = R.drawable.charmander
         var resId = imgResId
         imageView.setImageResource(resId)
-
         view?.addView(imageView)
+    }
 
+    // turns off visibility of all tower placement buttons
+    private fun visibilityOff(buttons: ArrayList<Button>) {
+        for (b in buttons) {
+            b.visibility = View.INVISIBLE
+        }
     }
 }
