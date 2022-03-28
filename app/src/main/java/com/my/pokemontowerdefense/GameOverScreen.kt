8e8afb943fa22b138361ef50a90063cd068a1023
Chiranjeevi <cchimmili3@gatech.edu>
2022-03-28 15:43:29 -0400

    game over screen

---
+++ app/src/main/java/com/my/pokemontowerdefense/GameOverScreen.kt
@@ -0,0 +1,11 @@
+package com.my.pokemontowerdefense
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameOverScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_over_screen)
+    }
+}
